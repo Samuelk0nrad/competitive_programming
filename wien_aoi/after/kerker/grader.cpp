cbebeaee2461ef05782f2f8a89b6e9700885503b
SamuelK0nrad <samuel.ko702@gmail.com>
2026-03-07 09:48:31 +0100

    finish kerker

--- wien_aoi/after/kerker/grader.cpp
+++ wien_aoi/after/kerker/grader.cpp
@@ -1,40 +1,55 @@
+#include "kerker.h"
 #include <algorithm>
 #include <iostream>
-#include <set>
-#include "kerker.h"
+#include <random>
+#include <vector>
 
 using namespace std;
 
 static int cnt;
 
-std::vector<int> mark_lock(const std::vector<int> &q)
-{
-	cnt++;
-	cout << "?";
-	for (const int a : q)
-	{
-		cout << " " << a;
-	}
-	cout << endl;
+vector<int> res;
 
-	vector<int> answ(q.size());
-	for (int &a : answ)
-	{
-		cin >> a;
-	}
-	return answ;
+std::vector<int> mark_lock(const std::vector<int> &q) {
+  cnt++;
+  cout << "?";
+  for (const int a : q) {
+    cout << " " << a;
+  }
+
+  vector<int> answ(q.size());
+  for (int i = 0; i < q.size(); i++) {
+    answ[i] = res[q[i] - 1];
+  }
+  random_device rd;
+  mt19937 gen{rd()};
+  // shuffle(answ.begin(), answ.end(), gen);
+
+  cout << " :";
+
+  for (const int a : answ) {
+    cout << " " << a;
+  }
+
+  cout << endl;
+  return answ;
 }
 
-int main()
-{
-	int n;
-	cin >> n;
-	vector<int> arr = find_combination(n);
-	cout << "!";
-	for (const int a : arr)
-	{
-		cout << " " << a;
-	}
-	cout << endl;
-	cout << "Number of queries:" << cnt << endl;
+int main() {
+  int n;
+  cin >> n;
+  res.resize(n);
+  for (int i = 0; i < n; i++) {
+    // cin >> res[i];
+    res[i] = i + 1;
+  }
+  // mark_lock({1, 2});
+
+  vector<int> arr = find_combination(n);
+  cout << "!";
+  for (const int a : arr) {
+    cout << " " << a;
+  }
+  cout << endl;
+  cout << "Number of queries:" << cnt << endl;
 }
--- wien_aoi/after/kerker/kerker.cpp
+++ wien_aoi/after/kerker/kerker.cpp
@@ -2,12 +2,11 @@
 #include "grader.cpp"
 #include <bits/stdc++.h>
 #include <cmath>
-#include <vector>
 
 using namespace std;
 
 struct node {
-  set<int> list;
+  vector<int> list;
   int l = -1;
   int r = -1;
 };
@@ -15,27 +14,69 @@ struct node {
 int n;
 vector<node> seg;
 
-void build(int p, int r, int l) {
+void build(int p, int l, int r) {
   seg[p].l = l;
   seg[p].r = r;
   if (r == l) {
     return;
   }
-  build(p * 2, r, l / 2);
-  build(p * 2 + 1, r / 2 + 1, l);
+  build(p * 2, l, (r - l) / 2 + l);
+  build(p * 2 + 1, (r - l) / 2 + l + 1, r);
 }
 
 std::vector<int> find_combination(int n) {
   vector<int> res(n);
 
-  seg.reserve(n * 4 + 5);
+  seg.resize(n * 4 + 5);
   build(1, 1, n);
 
   for (int i = 1; i <= n; i++) {
-    seg[0].list.insert(i);
+    seg[1].list.push_back(i);
   }
 
-  for (int i = 1; i <= ceil(log(n)); i++) {
+  vector<int> mark;
+  vector<int> ans;
+  for (int d = 0; d < ceil(log2(n)); d++) {
+    int start = (1 << d);
+    int end = (1 << (d + 1)) - 1;
+    for (int p = start; p <= end; p++) {
+      if (seg[p].l == -1 || seg[p].l == seg[p].r) {
+        continue;
+      }
+      int m = (seg[p].r - seg[p].l) / 2 + seg[p].l;
+      for (int i = seg[p].l; i <= m; i++) {
+        mark.push_back(i);
+      }
+    }
+
+    ans = mark_lock(mark);
+    sort(ans.begin(), ans.end());
+
+    for (int p = start; p <= end; p++) {
+      if (seg[p].l == -1)
+        continue;
+      if (seg[p].l == seg[p].r) {
+        res[seg[p].l - 1] = seg[p].list[0];
+        continue;
+      }
+      for (int i : seg[p].list) {
+        if (binary_search(ans.begin(), ans.end(), i)) {
+          seg[p * 2].list.push_back(i);
+        } else {
+          seg[p * 2 + 1].list.push_back(i);
+        }
+      }
+    }
+
+    mark.resize(0);
+  }
+
+  for (node &i : seg) {
+    if (i.l == -1)
+      continue;
+    if (i.l == i.r && !i.list.empty()) {
+      res[i.l - 1] = i.list[0];
+    }
   }
 
   return res;
