ace11f8ed54bb27e0541610bb82532dc242a6929
SamuelK0nrad <samuel.ko702@gmail.com>
2026-03-24 21:53:06 +0100

    ...

--- usaco/bronze/complete_search/Sleeping.cpp
+++ usaco/bronze/complete_search/Sleeping.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 struct node {
-  int last, current, i, deeph;
-  bool operator<(const node &x) const { return deeph < x.deeph; }
-  bool operator>(const node &x) const { return deeph > x.deeph; }
+  int last, cur, i, w;
+  bool operator<(const node &x) const { return w + cur > x.w + cur; }
+  bool operator>(const node &x) const { return w + cur < x.w + cur; }
 };
 
 int main() {
@@ -22,32 +22,32 @@ int main() {
 
     // last, current, i, deeph
     priority_queue<node> s;
-    s.push({-1, a[0], 0, 0});
-    int res = INT_MAX;
+    s.push({-1, a[0], 1, 0});
 
     while (!s.empty()) {
       auto e = s.top();
       s.pop();
 
-      if (e.i + 1 < n && (e.last == e.current || e.last == -1)) {
-        res = e.deeph;
-      } else if (e.i + 1 < n) {
+      cout << e.i << " " << e.w << " " << e.last << " " << e.cur << "\n";
+      if (e.i >= n) {
+        if (e.last == e.cur || e.last == -1) {
+          cout << e.w << "\n";
+          break;
+        }
         continue;
       }
 
-      if (e.last < e.current && e.last != -1) {
+      if (e.last < e.cur && e.last != -1) {
         continue;
       }
 
-      if (e.i + 1 < n && (e.last == e.current || e.last == -1)) {
-        s.push(node{e.current, a[e.i + 1], e.i + 1, e.deeph});
+      if (e.i < n && (e.last == e.cur || e.last == -1)) {
+        s.push(node{e.cur, a[e.i], e.i + 1, e.w});
       }
 
-      if (e.i + 1 < n) {
-        s.push(node{e.last, e.current + a[e.i + 1], e.i + 1, e.deeph + 1});
+      if (e.i < n) {
+        s.push(node{e.last, e.cur + a[e.i], e.i + 1, e.w + 1});
       }
     }
-
-    cout << res << "\n";
   }
 }
